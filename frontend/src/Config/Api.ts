0c176d10fe23595c828aa48c19a03ff266d392a7
rameshwar0611 <rameshwardangi0611@gmail.com>
2026-02-22 15:17:56 +0530

    Updated backend API URL for production

--- frontend/src/Config/Api.ts
+++ frontend/src/Config/Api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const API_URL = "http://localhost:8080";
+export const API_URL = "https://e-commerce-backend-akcp.onrender.com";
 
 export const api = axios.create({
   baseURL: API_URL, 
