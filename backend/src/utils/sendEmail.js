694e6a89b4d1c26ce1cee97533038185ef407538
rameshwar0611 <rameshwardangi0611@gmail.com>
2026-02-22 23:38:49 +0530

    Changed SMTP port to 465 for Render

--- backend/src/utils/sendEmail.js
+++ backend/src/utils/sendEmail.js
@@ -5,8 +5,8 @@ async function sendVerificationEmail(to, subject, text) {
   try {
     const transporter = nodemailer.createTransport({
       host: "smtp.gmail.com",
-      port: 587,
-      secure: false,
+      port: 465,
+      secure: true,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
